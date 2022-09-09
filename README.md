3e22c4c1981de33953612be73e8afd5f5d0862b9
Nathan Files <nathanwfiles@gmail.com>
2022-09-09 13:10:06 -0400

    functional cli with arguments

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # frisbee
 simple static file server
+
+```
+frisbee 0.1.0
+Nathan Files <nathanwfiles@gmail.com>
+
+USAGE:
+    frisbee [OPTIONS]
+
+OPTIONS:
+    -h, --help               Print help information
+    -p, --public <public>    Path prefix for HTTP requests [default: /]
+    -r, --root <root>        Directory to serve from disk [default: .]
+    -V, --version            Print version information
+
+Port/Address/etc can be configured with Rocket's native configuration system
+https://rocket.rs/v0.5-rc/guide/configuration/
+
+ROCKET_PORT=80 frisbee
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+use rocket::fs::FileServer;
+
+extern crate clap;
+use clap::Parser;
+
+const AFTER_HELP: &str = "\
+Port/Address/etc can be configured with Rocket's native configuration system
+https://rocket.rs/v0.5-rc/guide/configuration/
+
+ROCKET_PORT=80 frisbee";
+
+#[derive(Parser, Debug)]
+#[clap(author, version, after_help = AFTER_HELP, long_about = None)]
+struct Args {
+    /// Directory to serve from disk
+    #[clap(short, long, value_parser,
+            value_name = "root",
+            value_hint = clap::ValueHint::DirPath,
+            default_value = ".")]
+    root: std::path::PathBuf,
+
+    /// Path prefix for HTTP requests
+    #[clap(short, long, value_name = "public", default_value = "/")]
+    public: String,
+}
+
+#[launch]
+fn rocket() -> _ {
+    let args = Args::parse();
+
+    rocket::build().mount(args.public, FileServer::from(args.root.as_path()))
 }
